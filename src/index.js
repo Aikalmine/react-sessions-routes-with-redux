3d8cb054ee023a472814c581b09af25bdc5640f9
M. Ali <m.ali@pixako.com>
2021-08-13 12:46:37 +0500

    fixed routing and authenticated issues

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+/* eslint-disable import/default */
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import { sessionService, sessionReducer } from 'redux-react-session';
+import thunkMiddleware from 'redux-thunk';
+import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
+// Add the sessionReducer
+const reducer = combineReducers({
+  session: sessionReducer
+});
+
+const store = createStore(reducer, undefined, compose(applyMiddleware(thunkMiddleware)));
+
+// Init the session service
+sessionService.initSessionService(store);
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>, document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
